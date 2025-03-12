f9349c861d4308bdfae81200e47fa6d190e408aa
Tara Lewis <t.lewis0220201@arts.ac.uk>
2025-03-12 10:40:51 +0000

    ticket-4-get-articles

--- models/articles.models.js
+++ models/articles.models.js
@@ -16,3 +16,20 @@ exports.fetchArticleByID = (article_id) => {
       return rows[0];
     });
 };
+
+exports.fetchAllArticles = () => {
+  return db
+    .query(
+      `SELECT articles.article_id, articles.title, articles.author, articles.topic, 
+    articles.created_at, articles.votes, articles.article_img_url,
+    COUNT(comments.comment_id):: INT AS comment_count
+    FROM articles
+    LEFT JOIN comments ON articles.article_id = comments.article_id
+    GROUP BY articles.article_id
+    ORDER BY articles.created_at DESC;`
+    )
+    .then(({ rows }) => {
+      console.log("hello");
+      return rows;
+    });
+};
